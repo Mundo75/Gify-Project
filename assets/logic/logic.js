c8aef19fff8e2e26a58cc7494212e87799f60fc9
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-03-24 19:00:00 -0700

    Gifs loading to html, but not in cards

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
-   
-    //set up array of baseball teams to use as starting set-up for page
     var teams = ["arizona diamondbacks", "new york yankees", "seattle mariners", "cincinnati reds", "miami marlins", "tampa bay rays", "minnesota twins", "san diego padres", "texas rangers", "kansas city royals"];
+    
     //loop through baseball array, adding buttons to html using jquery
 
     function initialSetup () {
@@ -30,48 +29,50 @@ $(document).ready(function() {
     })
     
     //write cote to parse JSON data and append the gif images to the HTML
-    $(".teamName").on("click")
+    $(document).on("click", ".teamName", function() {
 
-    var apiKeyParameter =   "aEI26EVtx20kHGGHabgtZjXbZwtnPvot";
-    var mainURL =           "https://api.giphy.com/v1/gifs/search?"
-    var searchParameter =   $(this).attr("data-name");
-    var queryURL =          mainURL + "api_key=" + apiKeyParameter + "&q=" + searchParameter + "&limit=9&offset=0&rating=PG-13&lang=en"
+        var apiKeyParameter =   "aEI26EVtx20kHGGHabgtZjXbZwtnPvot";
+        var mainURL =           "https://api.giphy.com/v1/gifs/search?"
+        var searchParameter =   $(this).attr("data-name");
+        var queryURL =          mainURL + "api_key=" + apiKeyParameter + "&q=" + searchParameter + "&limit=9&offset=0&rating=PG-13&lang=en"
 
-    $.ajax({
+        $.ajax({
 
-         url:    queryURL,
-         method: "GET"
+            url:    queryURL,
+            method: "GET"
 
-    }).then(function(response) {
+        }).then(function(response) {
 
-         console.log(response);
-         var teamData = response.data;
+            console.log(response);
+            var teamInfo = response.data;
 
-         for (var i = 0; i < teamData.length; i++) {
+            for (var i = 0; i < teamInfo.length; i++) {
 
-            var teamCard = $("<div class=\"card\">");
-            teamCard.attr("style", "width: 18rem");
-
-            var cardTitle = $("<div class=\"card-body\">");
-
-            var animated = results[i].images.fixed_height.url;
-            var still = results[i].images.fixed_height_still.url;
-
-            var teamGif = $("<img>")
-            teamGif.attr("src", still);
-            teamGif.attr("data-still", still);
-            teamGif.attr("data-animate", animated);
-            teamGif.attr("data-state", "still");
-            teamGif.addClass("teamImage");
-  
-            teamCard.append(teamGif);
-            $("gifSpace1").append(teamCard);
-
+                var teamCard = $("<div>");
             
+                var url1 = teamInfo[i].images.fixed_height.url;
+                var url2 = teamInfo[i].images.fixed_height_still.url;
 
-        };
+                var teamGif = $("<img>").attr("src", url1);
+                //teamGif.attr("src", still);
+                //teamGif.attr("data-still", still);
+                //teamGif.attr("data-animate", animated);
+                //teamGif.attr("data-state", "still");
+                //teamGif.addClass("teamImage");
+    
+                teamCard.append(teamGif);
+                $("#gifSpace1").append(teamCard);
 
-     })
+                
+
+            };
+
+        });
+
+    });
+
+        
+        
     //set up code to play/pause gifs as clicked
 
 });
